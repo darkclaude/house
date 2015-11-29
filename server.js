690f1cf4b6f89fe62d738f5c1c9055c992e486c6
Hamid <darkclaudevfx@gmail.com>
2015-11-29 21:25:57 +0000

    sdf

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = 80;
+var port  = process.env.OPENSHIFT_NODEJS_PORT;
 var state = 'f';
 app.use('/',express.static(__dirname + '/views'));
 app.set('view engine', 'ejs');
@@ -10,20 +10,5 @@ app.get('/', function  (req, res) {
 	res.render('index.ejs');
 });
 
-app.post('/control/on', function(req, res){
-state = 'o';
-res.send("ok");
-});
 
-app.post('/control/off', function(req, res){
-state='f';
-res.send("ok");
-});
-
-app.get('/arduino', function(req, res){
-res.send(state);
-req.params.s = "enable";
-console.log("Arduino is GeTting me oo! ");
-});
-console.log("Listening on "+port);
-app.listen(port);
+app.listen(port, process.env.OPENSHIFT_NODEJS_IP);
